c45e2bb36e3bddb29172edb09fcfe55a032ca301
unknown <S8019188@NB70196.dy.allianz-suisse.ch>
2015-09-30 18:37:22 +0200

    cleared merge mess

--- js/dataService.js
+++ js/dataService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('cdsApp', ['DataService']);
 angular.module('DataService', [])
 
@@ -33,43 +32,22 @@ angular.module('DataService', [])
                     {id: 4, name: 'name 4'}
                 ];
                 // perform some asynchronous operation, resolve or reject the promise when appropriate.
-                return $q(function(resolve) {
-                    setTimeout(function() {
+                return $q(function (resolve) {
+                    setTimeout(function () {
                         resolve(fakeData);
                     }, 2000);
                 });
             }
 
-            return asyncData().then(function(data) {
+            return asyncData().then(function (data) {
                 //everything went well
                 svc.items = data;
-            }, function(reason) {
+            }, function (reason) {
                 $log.warn('Failed: ' + reason);
             });
         };
 
         return svc;
     });
-=======
 
-angular.module('DataService', []).factory('DataService', function(){
-    var service = {};
-    service.topValue = "1 (default)";
-    service.middleValue = "2 (default)";
-    service.bottomValue;
-    /** If not found it will be created runtime */
 
-    service.currentUser = currentUser();
-
-    function currentUser(){
-        if( typeof current === "undefined"){
-            current = "current_value";
-        }
-        return current;
-    };
-
-    return service;
-});
-
-angular.module('cdsApp', ['DataService']);
->>>>>>> origin/master
--- js/demoControllers.js
+++ js/demoControllers.js
@@ -1,35 +1,23 @@
 angular
     .module('cdsApp')
-<<<<<<< HEAD
     .controller('GlobalController', GlobalController)
     .controller('MiddleController', MiddleController)
     .controller('BottomController', BottomController)
-=======
-    .controller('MiddleController', MiddleController)
-    .controller('BottomController', BottomController)
     .controller('NestedController', NestedController)
->>>>>>> origin/master
     .controller('TopController', TopController);
 
 /** Example of angular.extend() */
 function TopController(DataService) {
-
     var vm = {
         DataService: DataService,
         motto: 'Yes we can!'
     };
-
     angular.extend(this, vm);
-<<<<<<< HEAD
 };
-=======
-}
->>>>>>> origin/master
 
 function MiddleController(DataService) {
     var vm = this;
     vm.DataService = DataService;
-<<<<<<< HEAD
 };
 
 function BottomController(DataService, DataFactory) {
@@ -42,19 +30,14 @@ function GlobalController(DataService, DataFactory) {
     var vm = this;
     vm.DataService = DataService;
     vm.DataFactory = DataFactory;
+    
     DataFactory.getDataStream();
     vm.result = DataFactory.items;
 };
-=======
-}
 
-function BottomController(DataService) {
-    var vm = this;
-    vm.DataService = DataService;
-}
 
 function NestedController(DataService) {
     var vm = this;
     vm.DataService = DataService;
-}
->>>>>>> origin/master
+};
+
